973b98578521d21f9290397ac2296a0f1c19140c
Mutai Mwiti <mutaimwiti40@gmail.com>
2021-01-04 11:14:20 +0300

    add and configure eslint and prettier

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const operators = {$or: 'some', $and: 'every'};
+const operators = { $or: 'some', $and: 'every' };
 
-const getFnArgs = (value) => Array.isArray(value) ? value : [value];
+const getFnArgs = (value) => (Array.isArray(value) ? value : [value]);
 
 export const boolExec = (expression, options = {}) => {
-    const {fns = {}} = options;
+  const { fns = {} } = options;
 
-    const evaluate = (exp) => {
-        if (typeof exp === 'boolean') return exp;
-        if (typeof exp === 'function') return exp();
-        if (!exp || typeof exp !== 'object') return false;
+  const evaluate = (exp) => {
+    if (typeof exp === 'boolean') return exp;
+    if (typeof exp === 'function') return exp();
+    if (!exp || typeof exp !== 'object') return false;
 
-        const [key, value] = Object.entries(exp)[0];
+    const [key, value] = Object.entries(exp)[0];
 
-        if (key in operators) return value[operators[key]](evaluate);
-        if (key in fns) return fns[key](...getFnArgs(value));
-        if (key) throw Error(`Undefined operation: ${key}`);
+    if (key in operators) return value[operators[key]](evaluate);
+    if (key in fns) return fns[key](...getFnArgs(value));
+    if (key) throw Error(`Undefined operation: ${key}`);
 
-        return false;
-    };
+    return false;
+  };
 
-    return evaluate(expression);
-}
+  return evaluate(expression);
+};
--- index.test.js
+++ index.test.js
@@ -1,299 +1,276 @@
-import {boolExec} from "./index";
+import { boolExec } from './index';
 
 describe('boolExec()', function () {
+  it('should return false by default', () => {
+    expect(boolExec()).toEqual(false);
+  });
 
-    it('should return false by default', () => {
+  describe('callback expressions', () => {
+    it('should return correct value', () => {
+      let cb = () => true;
 
-        expect(boolExec()).toEqual(false);
+      expect(boolExec(cb)).toEqual(true);
 
+      cb = () => false;
+
+      expect(boolExec(cb)).toEqual(false);
+    });
+  });
+
+  describe('AND expression', () => {
+    it('should return correct value for simple booleans', () => {
+      let expression = { $and: [true, true] };
+      expect(boolExec(expression)).toEqual(true);
+
+      expression = { $and: [true, false] };
+      expect(boolExec(expression)).toEqual(false);
+
+      expression = { $and: [false, true] };
+      expect(boolExec(expression)).toEqual(false);
+
+      expression = { $and: [false, false] };
+      expect(boolExec(expression)).toEqual(false);
+    });
+  });
+
+  describe('OR expression', () => {
+    it('should return correct value for simple booleans', () => {
+      let expression = { $or: [true, true] };
+      expect(boolExec(expression)).toEqual(true);
+
+      expression = { $or: [true, false] };
+      expect(boolExec(expression)).toEqual(true);
+
+      expression = { $or: [false, true] };
+      expect(boolExec(expression)).toEqual(true);
+
+      expression = { $or: [false, false] };
+      expect(boolExec(expression)).toEqual(false);
+    });
+  });
+
+  describe('fn expression', () => {
+    it('should return correct value', () => {
+      const options = {
+        fns: {
+          isEven: (number) => number % 2 === 0,
+        },
+      };
+
+      let expression = { isEven: 7 };
+      expect(boolExec(expression, options)).toEqual(false);
+
+      expression = { isEven: 6 };
+      expect(boolExec(expression, options)).toEqual(true);
+    });
+  });
+
+  describe('compound expression', () => {
+    describe('AND - OR expression', () => {
+      it('should return correct values ', () => {
+        // scenario 1
+        let expression = {
+          $or: [{ $and: [true, true] }, false],
+        };
+        expect(boolExec(expression)).toEqual(true);
+
+        // scenario 2
+        expression = {
+          $or: [{ $and: [true, false] }, false],
+        };
+        expect(boolExec(expression)).toEqual(false);
+
+        // scenario 3
+        expression = {
+          $and: [true, { $or: [false, true] }],
+        };
+        expect(boolExec(expression)).toEqual(true);
+
+        // scenario 4
+        expression = {
+          $and: [false, { $or: [false, true] }],
+        };
+        expect(boolExec(expression)).toEqual(false);
+      });
     });
 
-    describe('callback expressions', () => {
-        it('should return correct value', () => {
-            let cb = () => true;
+    describe('OR - callback expression', () => {
+      it('should return correct values ', () => {
+        let expression = {
+          $or: [() => true, true],
+        };
+        expect(boolExec(expression)).toEqual(true);
 
-            expect(boolExec(cb)).toEqual(true);
+        // scenario 2
+        expression = {
+          $or: [true, () => false],
+        };
+        expect(boolExec(expression)).toEqual(true);
 
-            cb = () => false;
+        // scenario 3
+        expression = {
+          $or: [() => false, () => true],
+        };
+        expect(boolExec(expression)).toEqual(true);
 
-            expect(boolExec(cb)).toEqual(false);
-        });
+        // scenario 4
+        expression = {
+          $or: [() => false, () => false],
+        };
+        expect(boolExec(expression)).toEqual(false);
+      });
     });
 
-    describe('AND expression', () => {
-        it('should return correct value for simple booleans', () => {
-            let expression = {$and: [true, true]}
-            expect(boolExec(expression)).toEqual(true);
+    describe('AND - callback expression', () => {
+      it('should return correct values ', () => {
+        let expression = {
+          $and: [() => true, true],
+        };
+        expect(boolExec(expression)).toEqual(true);
 
-            expression = {$and: [true, false]}
-            expect(boolExec(expression)).toEqual(false);
+        // scenario 2
+        expression = {
+          $and: [true, () => false],
+        };
+        expect(boolExec(expression)).toEqual(false);
 
-            expression = {$and: [false, true]}
-            expect(boolExec(expression)).toEqual(false);
+        // scenario 3
+        expression = {
+          $and: [() => false, () => true],
+        };
+        expect(boolExec(expression)).toEqual(false);
 
-            expression = {$and: [false, false]}
-            expect(boolExec(expression)).toEqual(false);
-        });
+        // scenario 4
+        expression = {
+          $and: [() => false, () => false],
+        };
+        expect(boolExec(expression)).toEqual(false);
+      });
     });
 
-    describe('OR expression', () => {
-        it('should return correct value for simple booleans', () => {
-            let expression = {$or: [true, true]}
-            expect(boolExec(expression)).toEqual(true);
+    describe('AND - OR - callback expression', () => {
+      it('should return correct values ', () => {
+        // scenario 1
+        let expression = {
+          $and: [() => true, { $or: [false, () => true] }],
+        };
+        expect(boolExec(expression)).toEqual(true);
 
-            expression = {$or: [true, false]}
-            expect(boolExec(expression)).toEqual(true);
+        // scenario 2
+        expression = {
+          $and: [() => false, { $or: [true, false] }],
+        };
+        expect(boolExec(expression)).toEqual(false);
 
-            expression = {$or: [false, true]}
-            expect(boolExec(expression)).toEqual(true);
+        // scenario 3
+        expression = {
+          $or: [() => false, false, { $and: [true, true] }],
+        };
+        expect(boolExec(expression)).toEqual(true);
 
-            expression = {$or: [false, false]}
-            expect(boolExec(expression)).toEqual(false);
-        });
+        // scenario 4
+        expression = {
+          $or: [() => false, false, { $and: [true, false] }],
+        };
+        expect(boolExec(expression)).toEqual(false);
+
+        // scenario 5
+        expression = {
+          $or: [
+            () => false,
+            false,
+            {
+              $and: [
+                true,
+                {
+                  $or: [() => true, false],
+                },
+              ],
+            },
+          ],
+        };
+        expect(boolExec(expression)).toEqual(true);
+      });
     });
 
-    describe('fn expression', () => {
-        it('should return correct value', () => {
-            const options = {
-                fns: {
-                    isEven: number => number % 2 === 0
-                }
-            };
+    describe('AND - OR - fn - callback expression', () => {
+      it('should return correct values ', () => {
+        const options = {
+          fns: {
+            any: (target, values) => values.includes(target),
+            all: (targets, values) =>
+              targets.every((target) => values.includes(target)),
+          },
+        };
+        // scenario 1
+        let expression = {
+          $and: [
+            () => true,
+            { $or: [false, () => true] },
+            { any: [5, [1, 3, 4, 5, 6]] },
+          ],
+        };
+        expect(boolExec(expression, options)).toEqual(true);
 
-            let expression = {isEven: 7};
-            expect(boolExec(expression, options)).toEqual(false);
+        // scenario 2
+        expression = {
+          $and: [
+            () => true,
+            { $or: [true, false] },
+            { any: [5, [1, 3, 4, 6, 7]] },
+          ],
+        };
+        expect(boolExec(expression, options)).toEqual(false);
 
-            expression = {isEven: 6};
-            expect(boolExec(expression, options)).toEqual(true);
-        });
+        // scenario 3
+        expression = {
+          $or: [
+            () => false,
+            false,
+            { $and: [true, false] },
+            {
+              all: [
+                [3, 4, 7],
+                [2, 3, 4, 5, 6, 7],
+              ],
+            },
+          ],
+        };
+        expect(boolExec(expression, options)).toEqual(true);
+
+        // scenario 4
+        expression = {
+          $or: [
+            () => false,
+            false,
+            { $and: [true, false] },
+            {
+              all: [
+                [3, 7, 9],
+                [2, 3, 4, 5, 6],
+              ],
+            },
+          ],
+        };
+        expect(boolExec(expression, options)).toEqual(false);
+
+        // scenario 5
+        expression = {
+          $or: [
+            () => false,
+            false,
+            {
+              $and: [
+                true,
+                {
+                  $and: [true, () => true, { any: [2, [5, 2, 3, 4]] }],
+                },
+              ],
+            },
+          ],
+        };
+        expect(boolExec(expression, options)).toEqual(true);
+      });
     });
-
-    describe('compound expression', () => {
-        describe('AND - OR expression', () => {
-            it('should return correct values ', () => {
-                // scenario 1
-                let expression = {
-                    $or: [
-                        {$and: [true, true]},
-                        false
-                    ]
-                };
-                expect(boolExec(expression)).toEqual(true);
-
-                // scenario 2
-                expression = {
-                    $or: [
-                        {$and: [true, false]},
-                        false
-                    ]
-                };
-                expect(boolExec(expression)).toEqual(false);
-
-                // scenario 3
-                expression = {
-                    $and: [
-                        true,
-                        {$or: [false, true]}
-                    ]
-                }
-                expect(boolExec(expression)).toEqual(true);
-
-                // scenario 4
-                expression = {
-                    $and: [
-                        false,
-                        {$or: [false, true]}
-                    ]
-                }
-                expect(boolExec(expression)).toEqual(false);
-            });
-        });
-
-        describe('OR - callback expression', () => {
-            it('should return correct values ', () => {
-                let expression = {
-                    $or: [() => true, true]
-                }
-                expect(boolExec(expression)).toEqual(true);
-
-                // scenario 2
-                expression = {
-                    $or: [true, () => false]
-                }
-                expect(boolExec(expression)).toEqual(true);
-
-                // scenario 3
-                expression = {
-                    $or: [() => false, () => true]
-                }
-                expect(boolExec(expression)).toEqual(true);
-
-                // scenario 4
-                expression = {
-                    $or: [() => false, () => false]
-                }
-                expect(boolExec(expression)).toEqual(false);
-            });
-        });
-
-        describe('AND - callback expression', () => {
-            it('should return correct values ', () => {
-                let expression = {
-                    $and: [() => true, true]
-                }
-                expect(boolExec(expression)).toEqual(true);
-
-                // scenario 2
-                expression = {
-                    $and: [true, () => false]
-                }
-                expect(boolExec(expression)).toEqual(false);
-
-                // scenario 3
-                expression = {
-                    $and: [() => false, () => true]
-                }
-                expect(boolExec(expression)).toEqual(false);
-
-                // scenario 4
-                expression = {
-                    $and: [() => false, () => false]
-                }
-                expect(boolExec(expression)).toEqual(false);
-            });
-        });
-
-        describe('AND - OR - callback expression', () => {
-            it('should return correct values ', () => {
-                // scenario 1
-                let expression = {
-                    $and: [
-                        () => true,
-                        {$or: [false, () => true]}
-                    ]
-                }
-                expect(boolExec(expression)).toEqual(true);
-
-                // scenario 2
-                expression = {
-                    $and: [
-                        () => false,
-                        {$or: [true, false]}
-                    ]
-                }
-                expect(boolExec(expression)).toEqual(false);
-
-                // scenario 3
-                expression = {
-                    $or: [
-                        () => false,
-                        false,
-                        {$and: [true, true]}
-                    ]
-                }
-                expect(boolExec(expression)).toEqual(true);
-
-                // scenario 4
-                expression = {
-                    $or: [
-                        () => false,
-                        false,
-                        {$and: [true, false]}
-                    ]
-                }
-                expect(boolExec(expression)).toEqual(false);
-
-                // scenario 5
-                expression = {
-                    $or: [
-                        () => false,
-                        false,
-                        {
-                            $and: [
-                                true,
-                                {
-                                    $or: [() => true, false]
-                                }
-                            ]
-                        }
-                    ]
-                }
-                expect(boolExec(expression)).toEqual(true);
-            });
-        });
-
-        describe('AND - OR - fn - callback expression', () => {
-            it('should return correct values ', () => {
-                const options = {
-                    fns: {
-                        any: (target, values) => values.includes(target),
-                        all: (targets, values) => targets.every(target => values.includes(target)),
-                    }
-                }
-                // scenario 1
-                let expression = {
-                    $and: [
-                        () => true,
-                        {$or: [false, () => true]},
-                        {any: [5, [1, 3, 4, 5, 6]]}
-                    ]
-                }
-                expect(boolExec(expression, options)).toEqual(true);
-
-                // scenario 2
-                expression = {
-                    $and: [
-                        () => true,
-                        {$or: [true, false]},
-                        {any: [5, [1, 3, 4, 6, 7]]}
-                    ]
-                }
-                expect(boolExec(expression, options)).toEqual(false);
-
-                // scenario 3
-                expression = {
-                    $or: [
-                        () => false,
-                        false,
-                        {$and: [true, false]},
-                        {all: [[3, 4, 7], [2, 3, 4, 5, 6, 7]]}
-                    ]
-                }
-                expect(boolExec(expression, options)).toEqual(true);
-
-                // scenario 4
-                expression = {
-                    $or: [
-                        () => false,
-                        false,
-                        {$and: [true, false]},
-                        {all: [[3, 7, 9], [2, 3, 4, 5, 6]]}
-                    ]
-                }
-                expect(boolExec(expression, options)).toEqual(false);
-
-                // scenario 5
-                expression = {
-                    $or: [
-                        () => false,
-                        false,
-                        {
-                            $and: [
-                                true,
-                                {
-                                    $and: [
-                                        true,
-                                        () => true,
-                                        {any: [2, [5, 2, 3, 4]]},
-                                    ]
-                                }
-                            ]
-                        }
-                    ]
-                }
-                expect(boolExec(expression, options)).toEqual(true);
-            });
-        });
-
-    });
+  });
 });
